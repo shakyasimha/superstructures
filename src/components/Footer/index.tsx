a458a15cbca51699c9233a92f7970dbd82faa48c
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-04-18 22:14:15 +0545

    fixed errors; should be deployed now

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Link from "next/link";
+// import Link from "next/link";
 import { montserrat } from "@/app/ui/fonts";
 
 export default function Page() {
     return(
         <>
-            <p> This is a footer </p>
+            <p className={`${montserrat.className}`}> This is a footer </p>
         </>
     )
 }
