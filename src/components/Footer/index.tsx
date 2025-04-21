64b6eb2b2942d8b33e38b6f2433e054e9b71be7c
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-04-21 23:38:14 +0545

    added contacts in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@
 
 // import Link from "next/link";
 import { montserrat } from "@/app/ui/fonts";
+import Link from "next/link";
 
 export default function Page() {
     return(
@@ -10,6 +11,22 @@ export default function Page() {
                 {/* Company info */}
                 <div className="flex flex-col justify-between px-4 py-4 ">
                     <div className="text-xl">Superstructures, Pvt. Ltd.</div>
+                    
+                    <Link 
+                        key="location"
+                        href="https://maps.app.goo.gl/ETaB1HxrvrUxeT4y8"
+                        className="text-base hover:text-[#ddd] transition duration-200"
+                    >
+                        Krishna Galli, Lalitpur-11, Bagmati, Nepal
+                    </Link>
+                    
+                    <Link
+                        key="email"
+                        href="mailto:superstructures@gmail.com"
+                        className="text-base hover:text-[#ddd] transition duration-200"
+                    > 
+                        superstructures@gmail.com
+                    </Link>
                 </div>
             </div>
         </footer>
