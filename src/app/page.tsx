ed2858312bb17b84e8503da79f79a0e79f1b6b54
Sushovan Shakya <sushakya235@proton.me>
2025-05-23 17:30:55 +0545

    working on homepage slides

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ export default function Home() {
   // const name: string = "Superstructures";
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        Superstructures Homepage
-
+    <div className="w-full h-full flex flex-col flex-grow">
         <Slideshow />
+        
+        <div className="flex flex-col items-center m-auto">
+          Superstructures Inc
+        </div>
     </div>
   );
 }
--- src/components/Slideshow/index.tsx
+++ src/components/Slideshow/index.tsx
@@ -1,14 +1,47 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Slideshow() {
-    const [nextImage, setNextImage] = useState(0);
+    const images = [
+        "1008.jpg",
+        "1009.jpg",
+        "1010.jpg",
+        "1011.jpg"
+    ]
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fade, setFade] = useState(false);
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+           setFade(false);
+           
+           setTimeout(()=>{
+                setCurrentIndex((prev)=> (prev+1) % images.length);
+                setFade(true);  // set fade in
+           }, 200); // match fade-in-out duration
+        }, 3000); // change every 3s
+
+        return ()=>clearInterval(interval);
+    }, []);
 
     return (
-        <div className="flex flex-grow">
-            <Image />
+        <div className="flex md:my-16 md:mx-auto sm:my-4 sm:mx-4">
+            <div className="flex flex-row items-center">
+                <div className="transition duration-300 hover:scale-105">
+                    <Image 
+                        key={images[currentIndex]}
+                        src={`/images/${images[currentIndex]}`}
+                        width={1280}
+                        height={960}
+                        alt="auto slideshow"
+                        className={`object-cover transition-opacity duration-200 ${fade ? 'opacity-100' : 'opacity-0'}`}
+                    />
+                </div>
+            </div>
+    
         </div>
     )
 }
